0e91750ed77afca06099bdf5d5513a86a1996a1c
Jakob Willforss <jakob.willforss@hotmail.com>
2017-08-03 16:29:12 +0200

    Omit wrapper index in output when present to circumvent issue with writing proper annotation

--- SubsetMzML.py
+++ SubsetMzML.py
@@ -10,15 +10,16 @@ NAMESPACE = "http://psi.hupo.org/ms/mzml"
 def main():
 
     args = parse_arguments()
-
     register_namespaces()
     tree = et.parse(args.input)
 
     if args.is_indexed:
         root = tree.getroot()
         mzml_root = root.find(prepend_ns('mzML'))
+        out_tree = et.ElementTree(mzml_root)
     else:
         mzml_root = tree.getroot()
+        out_tree = tree
 
     run_node = mzml_root.find(prepend_ns('run'))
     spectrum_list_node = run_node.find(prepend_ns('spectrumList'))
@@ -39,15 +40,14 @@ def main():
             print('Number of spectra after: {}'.format(len(spectrum_list_after)))
             print('MS1/MS2 after: {}/{}'.format(stats_dict_after['MS1'], stats_dict_after['MS2']))
 
-        tree.write(args.output)
+        out_tree.write(args.output)
 
 
 def register_namespaces():
     et.register_namespace('', NAMESPACE)
     et.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
     et.register_namespace('schemaLocation', "http://psi.hupo.org/ms/mzml http://psidev.info/files/ms/mzML/xsd/mzML1.1.0.xsd")
-
-    et.register_namespace('schemaLocation', 'http://psi.hupo.org/ms/mzml http://psidev.info/files/ms/mzML/xsd/mzML1.1.2_idx.xsd')
+    # et.register_namespace('schemaLocation', 'http://psi.hupo.org/ms/mzml http://psidev.info/files/ms/mzML/xsd/mzML1.1.2_idx.xsd')
 
 
 def prepend_ns(s):
@@ -73,14 +73,11 @@ def calculate_stats_dict(spectrum_list, ms1_threshold=None):
                     stats_dict['ms1_threshold'] is None and \
                     stats_dict['MS1'] == ms1_threshold:
                 stats_dict['ms1_threshold'] = total_spectras
-                print(stats_dict)
 
         elif spec_dict['ms level'] == '2':
             stats_dict['MS2'] += 1
         else:
             raise ValueError('Unknown MS level: {}'.format(spec_dict['ms level']))
-
-    print(stats_dict)
 
     return stats_dict
 
@@ -100,22 +97,16 @@ def reduce_spectras(spectrum_list_node, threshold):
 
     print('Threshold: {}'.format(threshold))
 
-    removed = 0
-
     remove_nodes = list()
 
     passed_spect = 0
     for child in spectrum_list_node:
         passed_spect += 1
         if passed_spect > threshold:
-            # spectrum_list_node.remove(child)
             remove_nodes.append(child)
 
     for node in remove_nodes:
-        removed += 1
         spectrum_list_node.remove(node)
-
-    print('Total removed: {}'.format(removed))
 
     return spectrum_list_node
 
@@ -129,6 +120,7 @@ def parse_arguments():
     parser.add_argument('--is_indexed', action='store_true', default=False)
 
     parser.add_argument('--ms1_count', type=int)
+    parser.add_argument('--tot_count', type=int)
 
     parser.add_argument('--check_only', action='store_true', help='Print information about sample')
 
